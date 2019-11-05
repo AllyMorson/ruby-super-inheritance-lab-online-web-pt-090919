53f279dd8aed21c66931e98c0d9d02a1342a6d9a
Ally Morson <ally.morson@gmail.com>
2019-11-05 17:43:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,10 @@
-class Student 
-  
+class Student
+
   def hello
     puts "Hey there! I'm so excited to learn stuff."
-  end 
-  
+  end
+
   def raise_hand
-    puts "Pick me!"
-  end 
+   puts "Pick me!"
+  end
 end
